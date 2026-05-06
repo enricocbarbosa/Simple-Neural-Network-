7fe9e9cc67aaed651c2ed0278bbed454661bd813
Enrico Barbosa <enricocbarbosa@gmail.com>
2026-05-06 01:15:19 -0300

    Type hints and fine adjustments

--- neuralnetwork.py
+++ neuralnetwork.py
@@ -9,32 +9,34 @@ class NeuralNetwork:
         for i in range(1, len(layers)):
             self.layers.append(Layer(layers[i], layers[i-1]))
 
-    def forward(self, inputs: list):
+    def forward(self, inputs: list) -> list:
         # Passing the input for each layer getting its output, return the output of the last layer
         for layer in self.layers:
             layer_output = layer.forward(inputs)
             inputs = layer_output.copy()
         return inputs
 
-    def backward(self, target, prediction, lr):
+    def backward(self, target: float, prediction: float, lr: float=0.001):
         gradients = [self.mse_gradient(target, prediction)]
         for layer in self.layers[::-1]:
             gradients = layer.backward(gradients, lr)
 
     @staticmethod
-    def mse_gradient(target, prediction):
+    def mse_gradient(target: float, prediction: float) -> float:
         return prediction - target
 
     @staticmethod
-    def mse(target, prediction):
+    def mse(target: float, prediction: float) -> float:
         return (prediction - target)**2
 
-    def train(self, inputs: list, targets: list, epochs: int=200, lr: float=0.01):
+    def train(self, inputs: list, targets: list, epochs: int=200, lr: float=0.001):
         for i in range(epochs):
             total_error = 0
             for j in range(len(inputs)):
                 output = self.forward(inputs[j])
-                total_error += self.mse(targets[j], output)
-                self.backward(targets[j], output, lr)
+                prediction = output[0]
+                target = targets[j][0]
+                total_error += self.mse(target, prediction)
+                self.backward(target, prediction, lr)
             if i % 20 == 0:
                 print(f"Epoch {i}: Loss = {total_error:.4f}")
